98431c9a70c4843f3c3e59bbec146fdfe44f398d
hannahschwen <hannahschwen@gmail.com>
2024-07-10 13:15:54 -0400

    started inquirer

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const inquirer = require('inquirer');
-
+const express = require('express');
+const { Pool } = require('pg');
+const app = express();
 const questions = [
   {
   type: 'list',
@@ -17,3 +19,44 @@ const questions = [
   },
 ];
  
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+const pool = new Pool(
+  {
+    user: 'postgres',
+    password: 'hannarue',
+    host: 'localhost',
+    database: 'employees_db'
+  }
+)
+
+function init () {
+  inquirer
+  .prompt(questions)
+  .then((responses) => {
+
+  })
+}
+
+
+// next  is switch statement for questions
+// how to get chart to show? console.chart(variable)
+
+// pool.query(sql, function (err, {rows}) {
+//   if (err) {
+//     console.log(err)
+//   }
+//   console.table(rows);
+// })
+
+// 'View All Employees', 
+// 'Add Employee',
+// 'Update Employee Role',
+// 'View all Roles',
+// 'Add Role',
+// 'View All Departments',
+// 'Add Department',
+// 'Quit'
+
+init();
--- server.js
+++ server.js
@@ -1,6 +1,7 @@
-const express = require('express');
+
 const app = express();
 const PORT = 3001;
+
 
 app.listen(PORT, () => 
   console.log(`App listening at http://localhost:${PORT}`));
