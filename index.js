f65c8bcd748ff09642256c28150f1f43006df731
hannahschwen <hannahschwen@gmail.com>
2024-07-13 19:42:32 -0400

    wrote cases to view and add employees

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
+// how is this working together with schema.sql? Pool connects to the database
+
 const inquirer = require('inquirer');
 const express = require('express');
 const { Pool } = require('pg');
 const app = express();
 
-// import function that gets
+
 const getEmployeeInfo = require('./lib/getEmployeeInfo');
 
 const questions = [
@@ -33,38 +35,62 @@ const pool = new Pool(
     host: 'localhost',
     database: 'employees_db'
   },
-  console.log('Connected to employee database')
+  console.log('Logged in to employee database')
 )
 
 pool.connect();
 
+
 function init () {
   inquirer
   .prompt(questions)
-  .then((responses) => {
-
-  })
-}
-
-
-// next  is switch statement for questions
-// how to get chart to show? console.chart(variable)
-
-function test() {
-  pool.query(function (err, {rows}) {
-    if (err) {
-      console.log(err)
+  .then((response) => {
+    let sql;
+    // After making a choice and the case runs, I want it to re-prompt I think. Look at video
+    switch(response.choice) {
+      case 'View All Employees': // works
+        sql = 'SELECT * FROM employee';
+        pool.query(sql, function (err, {rows}) {
+          console.table(rows);
+        })
+        break;
+      case 'Add Employee': // works
+        // uses getEmployeeInfo function to prompt for info and saves in a string for sql
+        // There's an extra variable here, I can just make employeeInfo be sql and whatever, can't explain but I know in my head, do it later
+        getEmployeeInfo().then((employeeInfo) => {
+          sql = employeeInfo;
+          console.log(sql);
+          pool.query(sql, (err) => {
+            if (err) {
+              console.log(err);
+              return;
+            }
+            console.log("Employee added");
+          })
+        })
+        // do a query thingy to put into database
+        break;
+      case 'Update Employee Role': // not started
+        // stuff
+        break;
+      case 'View all Roles':// not started
+        // stuff
+        break;
+      case 'Add Role':// not started
+        // stuff
+        break;
+      case 'View All Departments':// not started
+        // stuff
+        break;
+      case 'Add Department':// not started
+        // stuff
+        break;
+      case 'Quit':// not started
+        // stuff
+        break;
     }
-    console.table(rows);
+    
   })
 }
-// 'View All Employees', 
-// 'Add Employee',
-// 'Update Employee Role',
-// 'View all Roles',
-// 'Add Role',
-// 'View All Departments',
-// 'Add Department',
-// 'Quit'
 
 init();
